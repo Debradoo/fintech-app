d3ded47cf6a399154faac5bac2c1b1ddb77bbc1a
Doose Deborah Sen <doosedear@gmail.com>
2026-02-02 19:11:10 +0100

    day-2 - backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import userRouter from "./routes/user.routes";
+
+const app = express()
+
+app.use(express.json)
+
+app.get("/", (req, res)=>{
+    res.send("Fintech API Running......")
+});
+app.use("/api",userRouter)
+
+export default app;
