9ff3ae6eb743c4598dcb466f9b7405f33a901e0a
Doose Deborah Sen <doosedear@gmail.com>
2026-02-10 14:22:00 +0100

    protected the route with authMiddleware

--- backend/src/routes/ai.route.ts
+++ backend/src/routes/ai.route.ts
@@ -1,9 +1,10 @@
 import expres from "express";
 import { askAI } from "../controllers/ai.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const aiRouter = expres.Router();
 
-aiRouter.post("/ask", askAI);
+aiRouter.post("/ask",authMiddleware, askAI);
 
 
 export default aiRouter;
